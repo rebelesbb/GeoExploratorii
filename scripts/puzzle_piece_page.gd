88123ce9e90ef8a02eadeb18b8f502a2bce517eb
Farcas Andrei <andreifarcas21@gmail.com>
2025-11-30 16:46:32 +0200

    Add puzzle pieces png format.

--- scripts/puzzle_piece_page.gd
+++ scripts/puzzle_piece_page.gd
@@ -1,11 +1,55 @@
-extends Node
+extends CanvasLayer
 
+signal piece_displayed_and_closed
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var puzzle_image = $CenterContainer/PanelContainer/VBoxContainer/PuzzlePieceImage 
+@onready var next_button = $CenterContainer/PanelContainer/VBoxContainer/MarginContainer/NextQuestionButton
 
+func display_confetti():
+	var conffeti_textures = [
+		load("res://assets/confetti/blue.png"),
+		load("res://assets/confetti/red.png"),
+		load("res://assets/confetti/yellow.png"),
+		load("res://assets/confetti/purple.png")
+	]
+	
+	var count = 500
+	var rnd = RandomNumberGenerator.new()
+	rnd.randomize()
+	var screen_size = get_viewport().get_visible_rect().size 
+	
+	for i in range(count):
+		var sprite = Sprite2D.new()
+		sprite.texture = conffeti_textures[rnd.randi_range(0, conffeti_textures.size() - 1)]
+		sprite.position = Vector2(
+			 rnd.randf_range(0, screen_size.x),
+			-20 
+		)
+		var scale = rnd.randf_range(0.02, 0.10)
+		sprite.scale = Vector2(scale, scale)
+		add_child(sprite)
+		
+		var tween = create_tween()
+		var velocity = Vector2(rnd.randf_range(-50, 50), rnd.randf_range(100, 300))
+		tween.tween_property(sprite, "position", sprite.position + velocity, 2.0)
+		tween.parallel().tween_property(sprite, "modulate:a", 0.0, 1.2).set_delay(1.0)
+		tween.tween_callback(sprite.queue_free)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# funcite apelata din quiz_page.gd
+func display_puzzle_piece(chapter_id: int, question_id: int):
+	var file_path = "res://assets/puzzle_pieces/Cap%d/%d.%d.png" % [chapter_id, chapter_id, question_id]
+	
+	var texture = load(file_path)
+	puzzle_image.texture = texture
+
+func _ready():
+	display_confetti()
+	
+	var texture := load("res://assets/puzzle_pieces/Cap2/2.1.png")
+	puzzle_image.texture = texture
+	puzzle_image.size = Vector2(300, 300)
+
+func _on_next_question_button_pressed():
+	emit_signal("piece_displayed_and_closed")
+	
+	queue_free()
